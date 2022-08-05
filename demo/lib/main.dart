12230b750005716b45ed734febe597bba92ad37b
田超 <tianchoy@live.com>
2022-08-06 00:03:23 +0800

    学习记录-0805

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -11,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter'),
+        title: const Text('Flutter-title'),
       ),
       body: const HomeContent(),
     ));
@@ -22,8 +20,21 @@ class HomeContent extends StatelessWidget {
   const HomeContent({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('hello. Flutter,I like !'),
+    return Center(
+      //child: Text('hello. Flutter,I like !'),
+      child: Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: Image.network(
+          "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png",
+          //color: Colors.red,
+          //fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
