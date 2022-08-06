bc623ee49e7334010ac635c5cd6d8f2eb7e18817
田超 <tianchoy@live.com>
2022-08-06 09:38:21 +0800

    loading inmage

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -21,20 +21,20 @@ class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      //child: Text('hello. Flutter,I like !'),
-      child: Container(
-        width: 100,
-        height: 100,
-        decoration: BoxDecoration(
-          color: Colors.yellow,
-          borderRadius: BorderRadius.circular(50),
-        ),
-        child: Image.network(
-          "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png",
-          //color: Colors.red,
-          //fit: BoxFit.cover,
-        ),
+        //child: Text('hello. Flutter,I like !'),
+        child: Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        borderRadius: BorderRadius.circular(50),
       ),
-    );
+      // child: Image.network(
+      //   "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png",
+      //   //color: Colors.red,
+      //   //fit: BoxFit.cover,
+      // ),
+      child: Image.asset('images/1.png'),
+    ));
   }
 }
