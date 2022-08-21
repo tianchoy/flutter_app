0061e2335987d2e8f70cdddc0057d063986ceddb
田超 <tianchoy@live.com>
2022-08-21 18:14:03 +0800

    test-router-part1

--- demo/lib/pages/tabs/home.dart
+++ demo/lib/pages/tabs/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../testRouter/Search.dart';
+import '../testRouter/form.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,18 +16,27 @@ class _HomePageState extends State<HomePage> {
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        RaisedButton(
-          child: Text('跳转至分类页面'),
-          onPressed: () {},
-          color: Theme.of(context).accentColor,
-          textTheme: ButtonTextTheme.primary,
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => const SearchPage(),
+            ));
+          },
+          style: ButtonStyle(
+              padding: MaterialStateProperty.all(
+                  const EdgeInsets.fromLTRB(20, 10, 20, 10))),
+          child: const Text('跳转至分类页面'),
         ),
-        SizedBox(height: 20.0),
-        RaisedButton(
-          child: Text('跳转至搜索页面并传值'),
-          onPressed: () {},
-          color: Theme.of(context).accentColor,
-          textTheme: ButtonTextTheme.primary,
+        const SizedBox(height: 20.0),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => FormPage(title: 'formPage')));
+          },
+          style: ButtonStyle(
+              padding: MaterialStateProperty.all(
+                  const EdgeInsets.fromLTRB(20, 10, 20, 10))),
+          child: const Text('跳转至搜索页面并传值'),
         )
       ],
     );
