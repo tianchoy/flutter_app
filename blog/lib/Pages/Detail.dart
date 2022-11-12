f9f58056e35603c05b46c873f4433ae2da5c68fd
田超 <tianchoy@live.com>
2022-11-12 21:45:58 +0800

    detail解决页面太长和打印报错的bug

--- blog/lib/Pages/Detail.dart
+++ blog/lib/Pages/Detail.dart
@@ -11,45 +11,80 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
-  // ignore: prefer_typing_uninitialized_variables
-  var getArt;
+  var getArt = {};
+  artDetail() {
+    String url = 'getArtDetail.php?id=' + Get.arguments["id"];
+    getDetail(url).then((res) {
+      if (res != Null) {
+        setState(() {
+          getArt = res;
+        });
+      }
+    });
+  }
+
   @override
   void initState() {
     super.initState();
-    // ignore: prefer_interpolation_to_compose_strings
-    String url = 'getArtDetail.php?id=' + Get.arguments["id"];
-    getDetail(url).then((res) {
-      setState(() {
-        getArt = res;
-      });
-    });
+    artDetail();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(getArt["title"]),
+        title: Text(
+          getArt["title"].toString(),
+        ),
       ),
-      body: Column(
+      body: ListView(
         children: [
           Padding(
-            padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+            padding: const EdgeInsets.all(20),
             child: RichText(
               text: HTML.toTextSpan(
                 context,
-                getArt["content"],
+                getArt["content"].toString(),
                 defaultTextStyle: TextStyle(
-                  color: Colors.grey[700],
-                  fontSize: 12,
-                  // etc etc
-                ),
+                    color: Colors.grey[700],
+                    fontSize: 12,
+                    decoration: TextDecoration.none
+                    // etc etc
+                    ),
               ),
             ),
           ),
           const SizedBox(
             height: 20,
           ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: 80,
+                  height: 80,
+                  color: Colors.red,
+                ),
+                const Spacer(
+                  flex: 1,
+                ),
+                Container(
+                  width: 80,
+                  height: 80,
+                  color: Colors.green,
+                ),
+                const Spacer(
+                  flex: 1,
+                ),
+                Container(
+                  width: 80,
+                  height: 80,
+                  color: Colors.yellow,
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
